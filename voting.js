32661cf838445dc1906e85d66f8c12cd52dae56a
David L <dlopez222@west-mec.org>
2021-04-21 15:21:32 -0700

    WIP

---
+++ voting.js
@@ -0,0 +1,15 @@
+function rickroll() {
+    document.getElementById("body").style.backgroundImage= "url('rickrollgif.gif')";
+    document.getElementById("html").style.backgroundImage= "url('rickrollgif.gif')";
+    var el_up = document.getElementById("GFG_UP");
+    var str = "Thanks For Not Voting For Your Canidate!!";
+    el_up.innerHTML = str;
+
+}
+function confetti() {
+    document.getElementById("body").style.backgroundImage= "url('confetti.gif')";
+    document.getElementById("html").style.backgroundImage= "url('confetti.gif')";
+    var el_up = document.getElementById("GFG_UP2");
+    var str = "Thanks For Voting For Your Canidate!!";
+    el_up.innerHTML = str;
+} 
